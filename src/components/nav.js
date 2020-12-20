6b487e7b396dc74d05339e013ca3fa9b2a2118d2
Rafal <orlowskirafa@gmail.com>
2020-12-20 21:05:15 +0100

    new

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 
 import React from "react"
 import { Link } from "gatsby"
-
+import MenuIcon from '@material-ui/icons/Menu';
 const Nav = () =>{
 
     return(
         <div className="Nav">
             <div className="container">
                 <div className="row v-center space-between">
-                    
+                    <MenuIcon/>
                     <div className="logo">
                     <Link to="404">Agency</Link>
                     </div>
