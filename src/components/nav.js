83e58fda856cc6b71200bcc56084e296358c493c
Rafal <orlowskirafa@gmail.com>
2020-12-20 22:56:26 +0100

    ss

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,33 @@
 
 import React from "react"
 import { Link } from "gatsby"
-import MenuIcon from '@material-ui/icons/Menu';
+
 const Nav = () =>{
 
     return(
         <div className="Nav">
             <div className="container">
                 <div className="row v-center space-between">
-                    <MenuIcon/>
+                    <div className="navInfo">
+                        <Link to="404">
+                            <p>All Prints</p>
+                        </Link>
+                        <Link to="404">
+                            <p>Process</p>
+                        </Link>
+                        <Link to="404">
+                            <p>About</p>
+                        </Link>
+                    </div>
+                    <div className="hamburger">
+                        <span></span>
+                        <span></span>
+                    </div>
                     <div className="logo">
-                    <Link to="404">Agency</Link>
+                    <Link to="404">Kuzniacki</Link>
+                    </div>
+                    <div className="Shop">
+                        <p>Shop</p>
                     </div>
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React,{useEffect} from "react"
+
 import { Link } from "gatsby"
 import Nav from "../components/nav"
-
+import Banner from "../components/Banner"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-<div>
-  <SEO title="Home" />
-  <Nav/>
+const IndexPage = () => {
 
-     
-</div>
+  useEffect(() =>{
+    let vh = window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh',`${vh}px`);
+  },[])
+
+  return(
+  <div className="Website">
+    <SEO title="Home" />
+    <Nav/>
+    <Banner/>
+      
+  </div>
+  )
+
+}
+  
 
 
-)
+
 
 export default IndexPage
