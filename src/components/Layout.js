6cb51451b6fe9cfc35213f4e96ec8734b3419054
Rafal <orlowskirafa@gmail.com>
2020-12-23 19:36:28 +0100

    s

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 import Nav from "../components/nav"
-
+import Footer from "../components/footer"
 const Layout = (props) =>{
 
     return(
         <div>
             <Nav/>
              {props.children}
+             <Footer/>
         </div>
            
     )
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const Footer = () =>{
+    return(
+        <div className=" container ">
+             <ul className="footerContainer">
+                <li className="footerItem">Shop</li>
+                <li className="footerItem">Process</li>
+                <li className="footerItem">About</li>
+                
+              </ul>
+        </div>
+       
+    )
+}
+
+export default Footer
