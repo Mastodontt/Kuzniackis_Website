9c32427b252a72914efd7ffe05a4b839536240ee
Rafal <orlowskirafa@gmail.com>
2020-12-30 21:28:10 +0100

    msa

--- src/components/BannerItems.js
+++ src/components/BannerItems.js
@@ -39,14 +39,38 @@ const BannerItems = () =>{
     <section className="container main">
       <div className="row2">
           <div className="BannerInfo">
+            
             <h1>An exploration in color and texture.</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Euismod elementum nisi quis eleifend quam. Vitae ultricies leo integer malesuada. Eu augue ut lectus arcu bibendum at. Non curabitur gravida arcu ac tortor dignissim.</p>
             <p className="UnderLine">Learn More</p>
           </div>
-         
-        <p> <Img fluid={data.Picture1.childImageSharp.fluid} /> </p>        
-        <p> <Img fluid={data.Picture2.childImageSharp.fluid} /> </p>
-        <p> <Img fluid={data.Picture3.childImageSharp.fluid} /> </p>
+         <div className="BannerItems">
+         <div className="Bannertext">
+         <h1>Canvas Drip / 1 of 3</h1>
+             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h3>
+             <p className="Button">Purchase a print</p>
+         </div>  
+            <p> <Img fluid={data.Picture1.childImageSharp.fluid} /> </p> 
+         </div>
+         <div className="BannerItems">
+            <div className="Bannertext">
+                <h1>Canvas Drip / 2 of 3</h1>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h3>
+                <p className="Button">Purchase a print</p>
+            </div>          
+            <p> <Img fluid={data.Picture2.childImageSharp.fluid} /> </p>
+         </div>
+         <div className="BannerItems">
+             
+         <div className="Bannertext">
+                <h1>Canvas Drip / 3 of 3</h1>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h3>
+                <p className="Button">Purchase a print</p>
+        </div>          
+            <p> <Img fluid={data.Picture3.childImageSharp.fluid} /> </p>
+         </div>
+    
+       
        
           
       </div>
